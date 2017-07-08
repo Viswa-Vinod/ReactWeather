0f1c9e48622d633fa9df5174e51fa90ae73fa095
Vinod Kumar <vkviswa@gmail.com>
2017-07-08 20:50:21 +0530

    Comment on middleware in server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+//the below middleware is required to divert traffic from https to http
 app.use(function(req, res, next){
 	if (req.headers['x-forwarded-proto']==='http') {
 		next();
 	} else {
 		res.redirect('http://' + req.hostname + req.url);
 	}
-})
+});
 
 app.listen(PORT,function(){
 	console.log('server running on '+ PORT);
