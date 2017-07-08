8af8b40dd67a24c7aa4f8202e99d67e1dd622da2
Vinod Kumar <vkviswa@gmail.com>
2017-07-08 21:30:23 +0530

    update middleware for localhost

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ app.use(express.static('public'));
 //the below middleware is required to divert traffic from https to http
 app.use(function(req, res, next){
 	if (req.headers['x-forwarded-proto']==='http') {
-		next();
-	} else {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		
+		next();
 	}
 });
 
