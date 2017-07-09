b23de4ccc205a1a80ce92014e352bae716365d20
Vinod Kumar <vkviswa@gmail.com>
2017-07-09 11:59:52 +0530

    style example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 
@@ -12,7 +13,18 @@ var React = require('react');
 
 var Examples = () => {
 	return (
-			<h3>Examples</h3>
+			<div>
+				<h1 className="text-center">Examples</h1>
+				<p>Here are a few example locations to try out</p>
+				<ol>
+					<li>
+						<Link to="/?location=Delhi">Delhi, India</Link>
+					</li>
+					<li>
+						<Link to="/?location=Bangalore">Bangalore,India</Link>
+					</li>
+				</ol>
+			</div>
 		);
 }
 module.exports = Examples;
